453be8aba72d833519551c7b408ad5b5afe8ecce
Samuel Shaw <samuelshaw2000@gmail.com>
2022-11-21 18:06:45 -0800

    Moved logo to assets

--- tutor-web/nuxt.config.js
+++ tutor-web/nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // Set file target to be /static
+  static: {
+    prefix: false
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - tutor-web',
