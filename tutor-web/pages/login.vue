dd7c33093503744040fef8f909dbdce6caf36a6c
Samuel Shaw <sshaw16@ewu.edu>
2022-11-22 17:18:52 -0800

    Added non-functioning login page and jwt token for managing roles

---
+++ tutor-web/pages/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          You should automatically be redirected. If not, <v-btn> click here </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class IndexPage extends Vue {
+  name: string = 'IndexPage'
+  buttonText: string = 'Inspire me!'
+  isLoading: boolean = false
+
+  changeButtonText() {
+    this.buttonText =
+      this.buttonText === 'Inspire me!' ? 'Inspire me again!' : 'Inspire me!'
+    this.isLoading = true
+    setTimeout(() => {
+      this.isLoading = false
+    }, 1000)
+  }
+}
+</script>
