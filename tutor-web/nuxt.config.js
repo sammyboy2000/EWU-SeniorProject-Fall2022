7de88bc537531b76a7d462d1c290ac5e208a8009
Samuel Shaw <samuelshaw2000@gmail.com>
2022-12-01 13:01:53 -0800

    Update nuxt.config.js

--- tutor-web/nuxt.config.js
+++ tutor-web/nuxt.config.js
@@ -49,7 +49,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://0.0.0.0:7125',
+    baseURL: 'http://localhost:7125',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
