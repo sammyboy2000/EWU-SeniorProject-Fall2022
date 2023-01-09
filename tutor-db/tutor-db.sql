929ae9da7e6b7789ca362eb1bc0b674faa8a9d66
Samuel Shaw <samuelshaw2000@gmail.com>
2023-01-08 16:31:41 -0800

    Update tutor-db.sql

--- tutor-db/tutor-db.sql
+++ tutor-db/tutor-db.sql
@@ -1,5 +1,20 @@
-CREATE DATABASE tutor_db;
+CREATE DATABASE IF NOT EXISTS tutor_db;
 USE tutor_db;
+
+#
+# TABLE STRUCTURE FOR: Users
+#
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `user_id` varchar(255) NOT NULL,
+  `external_id` varchar(255) NOT NULL,
+  `is_student` BIT(1) NOT NULL,
+  `is_tutor` BIT(1) NOT NULL,
+  `is_admin` BIT(1) NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
 # TABLE STRUCTURE FOR: Student
@@ -13,7 +28,7 @@ CREATE TABLE `Student` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `external_id` (`external_id`)
+  FOREIGN KEY (external_id) REFERENCES Users(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
@@ -28,7 +43,22 @@ CREATE TABLE `Tutor` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   KEY `id` (`id`),
-  KEY `external_id` (`external_id`)
+  FOREIGN KEY (external_id) REFERENCES Users(user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# TABLE STRUCTURE FOR: Admin
+#
+
+DROP TABLE IF EXISTS `Admin`;
+
+CREATE TABLE `Admin` (
+  `id` varchar(255) NOT NULL,
+  `external_id` varchar(255) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  KEY `id` (`id`),
+  FOREIGN KEY (external_id) REFERENCES Users(user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
@@ -54,7 +84,8 @@ CREATE TABLE `Topic` (
   `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
   `class_id` int(9) unsigned NOT NULL,
   `topic` varchar(255) NOT NULL,
-  KEY `id` (`id`)
+  KEY `id` (`id`),
+  FOREIGN KEY (class_id) REFERENCES Class(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
@@ -69,7 +100,9 @@ CREATE TABLE `Questions` (
   `class_id` int(9) NOT NULL,
   `topic_id` int(9) NOT NULL,
   `content` varchar(750) NOT NULL,
-  UNIQUE KEY `question_id` (`question_id`)
+  UNIQUE KEY `question_id` (`question_id`),
+  FOREIGN KEY (topic_id) REFERENCES Topic(id),
+  FOREIGN KEY (class_id) REFERENCES Class(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
@@ -86,13 +119,7 @@ CREATE TABLE `Answered_Questions` (
   `topic_id` varchar(255) NOT NULL,
   `content` varchar(750) NOT NULL,
   `response` varchar(1000) NOT NULL,
-  KEY `question_id` (`question_id`)
+  UNIQUE KEY `question_id` (`question_id`),
+  FOREIGN KEY (topic_id) REFERENCES Topic(id),
+  FOREIGN KEY (class_id) REFERENCES Class(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-
-
-
-
