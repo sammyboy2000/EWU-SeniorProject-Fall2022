2be076ce1f70bb491bc87804277c02edf12c6150
Samuel Shaw <samuelshaw2000@gmail.com>
2023-01-29 18:18:33 -0800

    Change connection string settings

--- Tutor.Api/Models/tutor_dbContext.cs
+++ Tutor.Api/Models/tutor_dbContext.cs
@@ -32,7 +32,7 @@ namespace Tutor.Api.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //Should only happen when in dev mode on windows and local engine isn't running. Will not work in Docker.
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=tutor_db;Integrated Security=True");
             }
         }
