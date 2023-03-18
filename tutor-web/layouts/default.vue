1777006918052403b23497d9afc5e0d5cad0d891
Samuel Shaw <sshaw16@ewu.edu>
2023-03-18 15:44:56 -0700

    Fixed Spacing

--- tutor-web/layouts/default.vue
+++ tutor-web/layouts/default.vue
@@ -8,7 +8,7 @@
         </v-btn>
       </v-toolbar-title>
       <v-spacer />
-      <v-text v-if="username != null">Welcome {{ username }}!</v-text>
+      <v-text v-if="username != null">Welcome{{ username }}!</v-text>
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
